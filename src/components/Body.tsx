adebf1477032bb886d6a7cfafe4a9dae41960f4e
Zewuck <deyner.higuera.26@gmail.com>
2024-02-24 19:54:30 -0500

    Proyecto Iniciado Con React,Vite,TypeScript y tailwind

---
+++ src/components/Body.tsx
@@ -0,0 +1,7 @@
+export function Body () {
+    return (
+        <div className="w-full justify-center text-center bg-emerald-200">
+            <h1 className="text-3xl">Un H1 desde components</h1>
+        </div>
+    )
+}
